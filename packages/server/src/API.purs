70dec2324481d8106392e6d2f6804cf085ccdfe4
kentookura <kento@okura.at>
2023-05-11 18:47:47 +0200

    remove unnecessary endpoints

--- packages/server/src/API.purs
+++ packages/server/src/API.purs
@@ -9,6 +9,7 @@ import Data.Generic.Rep (class Generic)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (Tuple(..))
 import Effect (Effect)
+import Effect.Aff (Aff)
 import Effect.Class.Console (logShow, log)
 import Effect.Uncurried (EffectFn2, runEffectFn2)
 import HTTPurple
@@ -18,109 +19,78 @@ import Node.FS.Aff (readFile)
 import Node.FS.Sync (readTextFile)
 import Node.Encoding (Encoding(..))
 import API.Options
+import HTTPurple.Json.Argonaut as Argonaut
+import Markup.Namespace
+import Markup.Parser (parseMarkup)
+import Markup.Syntax (Markup)
+
+type Reference = String
 
 data Route
   = Home
   | Src String
-  | Profile String
-  | Account String
-  | Doc String
-  | Search { q :: String, sorting :: Maybe Sort }
-  | SayHello
+  | Namespace String
+  | Doc Reference
 
 derive instance Generic Route _
-
-data Sort = Asc | Desc
-
-derive instance Generic Sort _
-
-sortToString :: Sort -> String
-sortToString = case _ of
-  Asc -> "asc"
-  Desc -> "desc"
-
-sortFromString :: String -> Either String Sort
-sortFromString = case _ of
-  "asc" -> Right Asc
-  "desc" -> Right Desc
-  val -> Left $ "Not a sort: " <> val
-
-sort :: RouteDuplex' String -> RouteDuplex' Sort
-sort = as sortToString sortFromString
-
-foreign import data Json :: Type
-
-foreign import parseJson :: String -> Maybe Json
-
-foreign import getName :: Json -> Maybe String
-
-type HelloWorldRequest = { name :: String }
-type HelloWorldResponse = { hello :: String }
-
-testDecoder :: JsonDecoder String HelloWorldRequest
-testDecoder = JsonDecoder fromJsonString
-  where
-  fromJsonString :: String -> Either String HelloWorldRequest
-  fromJsonString = (parseJson >=> getName) >>> map { name: _ } >>> note "Invalid json"
-
-testEncoder :: JsonEncoder HelloWorldResponse
-testEncoder = JsonEncoder $ \{ hello } -> "{\"hello\": \"" <> hello <> "\" }"
 
 api :: RouteDuplex' Route
 api = mkRoute
   { "Home": noArgs
   , "Src": "src" / string segment
-  , "Profile": "profile" / string segment
-  , "Account": "account" / string segment
-  , "Search": "search" ? { q: string, sorting: optional <<< sort }
-  , "SayHello": "hello" / noArgs
+  , "Namespace" : "namespaces" / string segment
   , "Doc" : "doc" / string segment
   }
 
-responseHeaders = header "Content-Type" "text/html"
+htmlHeader = header "Content-Type" "text/html"
+
+port :: Int
+port = 8080
 
 main :: ServerM
-main = serve { port: 8080 } { route: api, router: apiRouter }
+main = serve { port: port } { route: api, router: apiRouter }
 
+
+apiRouter :: forall r. { route :: Route | r } -> ResponseM
 apiRouter { route: Home } = do
   body <- readFile "./webapp/index.html"
-  ok' responseHeaders body
+  ok' htmlHeader body
+
 apiRouter { route: Src path } = do
   src <- readFile $ "./webapp/" <> path
   ok' (header "Content-Type" "text/javascript") src
-apiRouter { route: Profile profile } = ok $ "hello " <> profile <> "!"
-apiRouter { route: Account account } = found' redirect ""
-  where
-  reverseRoute = print api $ Profile account
-  redirect = headers { "Location": reverseRoute }
-apiRouter { route: Search { q, sorting } } = ok $ "searching for query " <> q <> " " <> case sorting of
-  Just Asc -> "ascending"
-  Just Desc -> "descending"
-  Nothing -> "defaulting to ascending"
-apiRouter {route: Doc string} = do
-  ok "asdf"
-apiRouter { route: SayHello, method: Post, body } = usingCont do
-  { name } :: HelloWorldRequest <- fromJson testDecoder body
-  ok' jsonHeaders $ toJson testEncoder $ { hello: name }
-apiRouter { route: SayHello } = notFound
 
-getDocument :: String -> Effect (Either Error (Doc BlockType))
+apiRouter {route: Namespace s} = do
+  ok' jsonHeaders $ toJson Argonaut.jsonEncoder $ getNamespace s
+
+apiRouter { route: Doc ref} = do
+  body <- readFile $ "./markdown/" <> ref <> ".md"
+  ok' (header "Content-Type" "text/plain") body
+
+getNamespace :: String -> Maybe (Tree Listing)
+getNamespace s
+  | s == "algebra" = Just algebra
+  | s == "analysis" = Just analysis
+  | otherwise = Nothing
+
+
+getDocument :: String -> Effect (Either Error Markup)
 getDocument path = do
   res <- readTextFile UTF8 path
-  case runParser res blockParser of
+  case parseMarkup res of
     Right m -> pure $ Right m
     Left (ParseError s _) -> pure $ Left (RequestContentError s)
 
+
+
 watchDirectories :: Options -> (String -> Array String) -> Effect Unit
 watchDirectories {sourceDirectories} glob = 
-    (runEffectFn2 gazeImpl)
-      (A.concatMap glob sourceDirectories)
-      (\(d :: String) -> do
-        docs <- getDocument d
-        case docs of 
-          Left l -> pure unit
-          Right r -> logShow r
-      ) 
+  (runEffectFn2 gazeImpl)
+    (A.concatMap glob sourceDirectories)
+    (\(d :: String) -> do
+      docs <- getDocument d
+      log "what to do"
+    ) 
 
 
 fileGlob :: String -> Array String
