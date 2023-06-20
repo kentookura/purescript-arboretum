0fe78c0de6e008826d528fe291bf12ab589b2004
kentookura <kento@okura.at>
2023-06-20 10:37:13 +0200

    add Route

---
+++ packages/frontend/src/Route.purs
@@ -0,0 +1,24 @@
+module Route
+  ( Route(..)
+  , route
+  ) where
+
+import Prelude
+import Data.Generic.Rep (class Generic)
+import Routing.Duplex (print, parse, RouteDuplex', root)
+import Routing.Duplex.Generic (noArgs, sum)
+import Routing.Duplex.Generic.Syntax ((/))
+
+data Route = Workspace | FourOhFour | About | Revisions
+
+derive instance Generic Route _
+derive instance Eq Route
+derive instance Ord Route
+
+route :: RouteDuplex' Route
+route = root $ sum
+  { "Workspace": "workspace" / noArgs
+  , "About": "about" / noArgs
+  , "Revisions" : "revisions" / noArgs
+  , "FourOhFour": "404" / noArgs
+  }
