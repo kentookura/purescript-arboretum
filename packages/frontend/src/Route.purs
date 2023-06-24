cc04cef79504ea9eef3be5dc9feb7d1590a060bb
Kento Okura <kentookura@gmail.com>
2023-06-24 17:49:49 +0200

    add more Routes

--- packages/frontend/src/Route.purs
+++ packages/frontend/src/Route.purs
@@ -9,7 +9,7 @@ import Routing.Duplex (print, parse, RouteDuplex', root)
 import Routing.Duplex.Generic (noArgs, sum)
 import Routing.Duplex.Generic.Syntax ((/))
 
-data Route = Workspace | FourOhFour | About | Revisions
+data Route = Workspace | FourOhFour | About | Revisions | Courses | Editor
 
 derive instance Generic Route _
 derive instance Eq Route
@@ -19,6 +19,8 @@ route :: RouteDuplex' Route
 route = root $ sum
   { "Workspace": "workspace" / noArgs
   , "About": "about" / noArgs
-  , "Revisions" : "revisions" / noArgs
+  , "Revisions": "revisions" / noArgs
+  , "Courses": "courses" / noArgs
+  , "Editor": "editor" / noArgs
   , "FourOhFour": "404" / noArgs
   }
