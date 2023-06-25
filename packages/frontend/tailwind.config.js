833d8058985e4e2a6e5b51eebb4cce30a3f00aec
Kento Okura <kentookura@gmail.com>
2023-06-25 10:19:09 +0200

    update packages and config

--- packages/frontend/tailwind.config.js
+++ packages/frontend/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,purs,html}"],
   theme: { extend: {} },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
--- packages/frontend/vite.config.js
+++ packages/frontend/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  define: {},
+  define: {
+    "process.env": {},
+  },
 });
